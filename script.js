1b3aeb58cce78b8c802a3fa9873263cd22994764
2107BswWux <87951437+2107BswWux@users.noreply.github.com>
2021-10-13 18:15:25 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("script is linked");
+
+
+
+const firstName = "Java";
+const lastName = "Script";
+const userName = "Java Script";
+const email = "email@gmail.com";
+const password = "password";
+
+// creating an object
+
+const user = {
+    // first we define a property and then we give it a value
+    firstName:"Java",
+    lastName:"Script",
+    userName:"Java Script",
+}
+console.log(user);
+
+// array is a set of data where we have multiple values
+const fruit = ['apple', 'banana', 'orange', 'passion fruit', 'fejoa'];
+// we can target a specifi value with the array by decleraing its index number
+console.log(fruit[1]);
+console.log(fruit);
+
+// ok so an array is a object as well sooo what does that mean an object holds different methods or functions that can be appended so lets have a look
+
+// fruit.pop();
+// console.log(fruit);
+// removes the last value in an array
+
+// fruit.push('pear');
+// console.log(fruit);
+// add a value to the end of the array
+
+// fruit.shift();
+// console.log(fruit);
+// removes the first value in an array
+
+// fruit.unshift('blue berry');
+// console.log(fruit);
+// adds a value to the start of an array
+
+
